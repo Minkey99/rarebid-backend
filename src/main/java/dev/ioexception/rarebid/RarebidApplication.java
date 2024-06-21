549bd0b0607dcce9816ca34f6a1b7b8ebd15068e
BHC-Chicken <tij05078@gmail.com>
2024-06-21 16:57:39 +0900

    init

---
+++ src/main/java/dev/ioexception/rarebid/RarebidApplication.java
@@ -0,0 +1,13 @@
+package dev.ioexception.rarebid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RarebidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RarebidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ioexception/rarebid/RarebidApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ioexception.rarebid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RarebidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
